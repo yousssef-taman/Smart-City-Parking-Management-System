ddfdb72a25f0f91fdf9f6fe26bb8cf798eecfb95
youssefMohamedTaman <yousseftaman11@gmail.com>
2024-12-25 18:19:33 +0200

    uploading gradle

---
+++ src/test/java/com/CSED/SmatCityParing/SmatCityParingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSED.SmatCityParing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmatCityParingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
