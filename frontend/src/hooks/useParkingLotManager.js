5486a4426a694edf28eb7864e45b088c16c038ea
mohamed kamal <kamalmohammed837@gmail.com>
2024-12-28 00:16:48 +0200

    hooks

---
+++ frontend/src/hooks/useParkingLotManager.js
@@ -0,0 +1,46 @@
+
+export function useParkingLotManager() {
+    const createLot = async (lotData,slots) => {
+        try {
+            const response = await fetch('http://localhost:8080/api/parkinglots', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(lotData),
+            });
+            if (!response.ok) throw new Error('Failed to create parking lot');
+            return await response.json();
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    };
+
+    const updateLot = async (lotData) => {
+        try {
+            const response = await fetch(`http://localhost:8080/api/parking-lots/${lotData.id}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(lotData),
+            });
+            if (!response.ok) throw new Error('Failed to update parking lot');
+            return await response.json();
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    };
+
+    const deleteLot = async (lotId) => {
+        try {
+            const response = await fetch(`http://localhost:8080/api/parking-lots/${lotId}`, {
+                method: 'DELETE',
+            });
+            if (!response.ok) throw new Error('Failed to delete parking lot');
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    };
+
+    return { createLot, updateLot, deleteLot };
+}
